0a637f596fef708fe569f7fad62b6f03b5fe46a2
Chris <148207385+Xencil@users.noreply.github.com>
2025-12-09 19:54:10 +0000

    forgot to add a variable

--- public/exerciseChart.js
+++ public/exerciseChart.js
@@ -1,6 +1,7 @@
 async function createChart(){
     try{
         const response =await fetch('/tracker/chartData')
+        const contentType =response.headers.get('content-type');
         if(!contentType ||!contentType.includes('application/json')){
         console.error(await response.text());
         return;
